77be4c4e3ece7948a147a806920710929e8b6f3a
league <league@imac-4.attlocal.net>
2018-03-30 18:30:47 -0700

    03/30 MazeMaker

--- src/MazeMaker.java
+++ src/MazeMaker.java
@@ -28,8 +28,28 @@ public class MazeMaker{
 
 	private static void selectNextPath(Cell currentCell) {
 		// mark current cell as visited
-
+		currentCell.setBeenVisited(true);
+		
 		// check for unvisited neighbors
+		Cell[] store = new Cell[4];
+		int x = 0;
+		if(currentCell.getX()<5-1&&!maze.getCell(currentCell.getX()+1, currentCell.getY()).hasBeenVisited()) {
+			store[x] = maze.getCell(currentCell.getX(), currentCell.getY());
+			x++;
+		}
+		if(currentCell.getX()>0&&!maze.getCell(currentCell.getX()-1, currentCell.getY()).hasBeenVisited()) {
+			store[x] = maze.getCell(currentCell.getX(), currentCell.getY());
+			x++;
+		}
+		if(currentCell.getY()<5-1&&!maze.getCell(currentCell.getX(), currentCell.getY()+1).hasBeenVisited()) {
+			store[x] = maze.getCell(currentCell.getX(), currentCell.getY());
+			x++;
+		}
+		if(currentCell.getY()>0&&!maze.getCell(currentCell.getX(), currentCell.getY()-1).hasBeenVisited()) {
+			store[x] = maze.getCell(currentCell.getX(), currentCell.getY());
+			x++;
+		}
+		System.out.println(x);
 		
 		// if has unvisited neighbors,
 			// select one at random.
@@ -45,10 +65,36 @@ public class MazeMaker{
 				// make that the current cell
 		
 				//call the selectNextPath method with the current cell
+		
+		if(x>0) {
+			int rand = (int)(Math.random()*(x+1));
+			uncheckedCells.push(store[rand]);
+			removeWalls(currentCell, store[rand]);
+			selectNextPath(store[rand]);
+		}
+		else if(uncheckedCells.size()>0){
+			Cell c = uncheckedCells.pop();
+			selectNextPath(c);
+		}
 	}
 
 	private static void removeWalls(Cell c1, Cell c2) {
-		
+		if(c1.getX()<c2.getX()) {
+			c1.setEastWall(false);
+			c2.setWestWall(false);
+		}
+		if(c1.getX()>c2.getX()) {
+			c2.setEastWall(false);
+			c1.setWestWall(false);
+		}
+		if(c1.getY()<c2.getY()) {
+			c1.setNorthWall(false);
+			c2.setSouthWall(false);
+		}
+		if(c1.getY()>c2.getY()) {
+			c2.setNorthWall(false);
+			c1.setSouthWall(false);
+		}
 	}
 
 	private static ArrayList<Cell> getUnvisitedNeighbors(Cell c) {
